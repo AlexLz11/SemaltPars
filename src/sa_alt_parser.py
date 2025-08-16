2128708106f91314ef338c981218572f9b219ba7
AlexLz11 <alex.lz11@yandex.ru>
2025-08-16 23:03:16 +0700

    2025-08-16 lz sa_alt_parser

--- src/sa_alt_parser.py
+++ src/sa_alt_parser.py
@@ -20,6 +20,8 @@ def get_soup(url, session = None, parser='lxml'):
 
 def get_price(tag, c_name, p_name):
     price = tag.select_one('div.product-item-price-current').text.strip(' ₽\n\t\r').replace('\xa0','')
+    if not price:
+        return ' -'
     try:
         price = float(price)
         return price
@@ -33,20 +35,18 @@ def get_name(name):
         name = name.replace(old, new)
     return name
 
-def get_main_links(url, scheme, session=None):
-    soup = get_soup(url, session)
-    url_tags = soup.select_one('ul.catalog-section-list-tile-list').select('a.catalog-section-list-item-link')
-    main_links = [scheme + a['href'] for a in url_tags]
-    return main_links
-
 def get_data(url, scheme, session=None):
     soup = get_soup(url, session)
     prod_data = []
     category_cont = soup.select_one('div.bx_catalog_tile')
-    category_name = soup.select_one('h1.bx_catalog_tile_category_title').text
+    category_name = soup.select_one('h1.bx_catalog_tile_category_title').text if soup.select_one('h1.bx_catalog_tile_category_title') else ''
     items_cont = soup.select_one('div.wrapper_product_items')
     if not items_cont:
-        cat_links = [scheme + tag.find('a')['href'] for tag in category_cont.select('h2.bx_catalog_tile_title')]
+        if not category_cont:
+            url_tags = soup.select('a.catalog-section-list-item-link')
+        else:
+            url_tags = soup.select('h2.bx_catalog_tile_title a')
+        cat_links = [scheme + a['href'] for a in url_tags]
         for link in cat_links:
             prod_data = prod_data + get_data(link, scheme, session)
         return prod_data
@@ -71,11 +71,8 @@ if __name__ == '__main__':
     start = time.time()
     start_url = 'https://altsemena.org/catalog/'
     scheme = 'https://altsemena.org'
-    with requests.Session() as rs:
-        main_links = get_main_links(start_url, scheme, rs)    
-        products = []
-        for link in main_links:
-            products = products + get_data(link, scheme, rs)
+    with requests.Session() as rs:   
+        products = get_data(start_url, scheme, rs)
 
     csv_header = ['Категория', 'Наименование товара', 'Стоимость, руб']
     curr_dt = datetime.now()
