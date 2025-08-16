e96ef54fd60a5ef2ee294891358a443a210cc2df
AlexLz11 <alex.lz11@yandex.ru>
2025-08-16 19:37:10 +0700

    2025-08-16 lz SA_ALT

--- src/main.py
+++ src/main.py
@@ -19,12 +19,9 @@ def get_soup(url, session = None, parser='lxml'):
     return bs(html.text, parser)
 
 def get_price(tag, c_name, p_name):
-    price = tag.select_one('span.products__item-price').text.strip(' ₽\n\t')
-    if price.isdigit():
-        return int(price)
+    price = tag.select_one('div.product-item-price-current').text.strip(' ₽\n\t\r').replace('\xa0','')
     try:
-        price_lst = price.split(' ₽')
-        price = int(price_lst[-1])
+        price = float(price)
         return price
     except Exception as e:
         print(f'Категория: {c_name}, товар: {p_name}. Некорректное значение стоимости - {price}')
@@ -36,44 +33,54 @@ def get_name(name):
         name = name.replace(old, new)
     return name
 
-def get_data(url, session = None):
+def get_main_links(url, scheme, session=None):
+    soup = get_soup(url, session)
+    url_tags = soup.select_one('ul.catalog-section-list-tile-list').select('a.catalog-section-list-item-link')
+    main_links = [scheme + a['href'] for a in url_tags]
+    return main_links
+
+def get_data(url, scheme, session=None):
     soup = get_soup(url, session)
     prod_data = []
-    category_cont = soup.select_one('div.category__grid')
-    category_name = soup.select_one('h1.catalog__heading,h1.page__heading').text
-    if category_cont:
-        cat_links = [a['href'] for a in category_cont.select('a.category__item')]
+    category_cont = soup.select_one('div.bx_catalog_tile')
+    category_name = soup.select_one('h1.bx_catalog_tile_category_title').text
+    items_cont = soup.select_one('div.wrapper_product_items')
+    if not items_cont:
+        cat_links = [scheme + tag.find('a')['href'] for tag in category_cont.select('h2.bx_catalog_tile_title')]
         for link in cat_links:
-            prod_data = prod_data + get_data(link, session)
+            prod_data = prod_data + get_data(link, scheme, session)
         return prod_data
     else:
-        pagination = soup.select_one('ul.pagination')
+        pagination = soup.select_one('div.bx-pagination')
         max_page = 1 if not pagination else len(pagination.select('li')) - 2
         page = 1
         while page <= max_page:
-            product_cont = soup.select_one('div.products__grid')
-            products = [] if not product_cont else [product_tag for product_tag in product_cont.select('div.products__item-desc')]
+            products = items_cont.select('div.product-item') if items_cont.select('div.product-item') else []
             for product in products:
-                p_name = get_name(product.select_one('a.products__item-title').text)
+                p_name = get_name(product.select_one('div.product-item-title a').text.strip())
                 p_price = get_price(product, category_name, p_name)
                 prod_data.append({'name': p_name, 'price': p_price})
             page += 1
             if page <= max_page:
-                next_link = url + f'?page={page}'
+                next_link = url + f'?PAGEN_1={page}'
                 soup = get_soup(next_link, session)
+                items_cont = soup.select_one('div.wrapper_product_items')
         return [{category_name: prod_data}]
 
 if __name__ == '__main__':
     start = time.time()
-    scheme = 'https://alsemya.ru/all-categories'
+    start_url = 'https://altsemena.org/catalog/'
+    scheme = 'https://altsemena.org'
     with requests.Session() as rs:
-
-        products = get_data(scheme, rs)
+        main_links = get_main_links(start_url, scheme, rs)    
+        products = []
+        for link in main_links:
+            products = products + get_data(link, scheme, rs)
 
     csv_header = ['Категория', 'Наименование товара', 'Стоимость, руб']
     curr_dt = datetime.now()
     curr_dt_str = curr_dt.strftime('%Y-%m-%d_%H%M%S')
-    with open(f'SA_MSK_product_list_{curr_dt_str}.csv', 'a', encoding='utf-8-sig', newline='') as ouf:
+    with open(f'SA_ALT_product_list_{curr_dt_str}.csv', 'a', encoding='utf-8-sig', newline='') as ouf:
         writer = csv.writer(ouf, delimiter=';')
         writer.writerow(csv_header)
         for category in products:
